5513edc7ae98ae04b3e3f069cab727d47b124625
Mounika Kuber <109682737+Mounikakuber@users.noreply.github.com>
2023-04-25 00:28:23 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,59 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: " How many wheels does car have ? ",
+    answer: "Four",
+    options: [
+      "Six",
+      "Five",
+      "Two",
+      "Four"
+    ]
+  },
+    {
+    numb: 2,
+    question: " Which is our National Animal ? ",
+    answer: "Tiger",
+    options: [
+      "Lion",
+      "Cat",
+      "Tiger",
+      "Elephant"
+    ]
+  },
+    {
+    numb: 3,
+    question: " how many days are there in a week? ",
+    answer: "7",
+    options: [
+      "4",
+      "8",
+      "7",
+      "6"
+    ]
+  },
+    {
+    numb: 4,
+    question: " 2 + 4 = ? ",
+    answer: "6",
+    options: [
+      "8",
+      "4",
+      "2",
+      "6"
+    ]
+  },
+    {
+    numb: 5,
+    question: " What is the colour of our National Flag ? ",
+    answer: "Saffron, White, Dark green",
+    options: [
+      "Red, Pink, Blue",
+      "Black, White, Grey",
+      "Saffron, White, Dark green",
+      "Yellow, Red, Green"
+    ]
+  },
+  
+];
